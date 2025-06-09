d27b82ed384dd14dc18ccf4a61231ecade968b8d
Jack Kim <bumk991217@gmail.com>
2025-06-09 22:21:27 +0900

    Fade for the pages

--- src/controllers/MainController.java
+++ src/controllers/MainController.java
@@ -5,6 +5,8 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.layout.StackPane;
 import javafx.scene.Node;
 import javafx.scene.control.Button;
+import javafx.animation.FadeTransition;
+import javafx.util.Duration;
 
 public class MainController {
     @FXML
@@ -26,8 +28,35 @@ public class MainController {
 
     private void switchPage(String fxml) {
         try {
-            Node node = FXMLLoader.load(getClass().getResource("/src/views/" + fxml));
-            contentPane.getChildren().setAll(node);
+            Node newNode = FXMLLoader.load(getClass().getResource("/src/views/" + fxml));
+            Node oldNode = contentPane.getChildren().isEmpty() ? null : contentPane.getChildren().get(0);
+
+            if (oldNode != null) {
+                // Fade out the old node
+                FadeTransition fadeOut = new FadeTransition(Duration.millis(200), oldNode);
+                fadeOut.setFromValue(1);
+                fadeOut.setToValue(0);
+                fadeOut.setOnFinished(e -> {
+                    // After fade out, replace with the new node
+                    contentPane.getChildren().setAll(newNode);
+
+                    // Fade in the new node
+                    FadeTransition fadeIn = new FadeTransition(Duration.millis(300), newNode);
+                    newNode.setOpacity(0);
+                    fadeIn.setFromValue(0);
+                    fadeIn.setToValue(1);
+                    fadeIn.play();
+                });
+                fadeOut.play();
+            } else {
+                // If no node, just show new with fade in
+                newNode.setOpacity(0);
+                contentPane.getChildren().setAll(newNode);
+                FadeTransition fadeIn = new FadeTransition(Duration.millis(300), newNode);
+                fadeIn.setFromValue(0);
+                fadeIn.setToValue(1);
+                fadeIn.play();
+            }
         } catch (Exception e) {
             e.printStackTrace();
         }
